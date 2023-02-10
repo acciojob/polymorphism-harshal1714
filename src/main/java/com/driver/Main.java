87fcd53861f27dc3d6d9c2fc7874bc8edf6c03cc
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-02-10 11:22:15 +0530

    my 1st commint in polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        Product p=new Product();
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+        public static void main(String[] args){
+        Product p=new Product();
+        p.product(5,10);
+        p.product(2,5,8);
+        p.product(5.5,2.5);
+    }
 }
